af423c2d209ae2f276951a8bf8238dff091e7d24
Ilija Tavchioski <20493797+iletavcioski@users.noreply.github.com>
2020-03-04 23:37:05 +0100

    Add files via upload

---
+++ CodeForces/299_2_A.cpp
@@ -0,0 +1,224 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+     if(n==13)
+    {
+        cout<<"thirteen"<<endl;
+        return 0;
+    }
+    else if(n==14)
+    {
+        cout<<"fourteen"<<endl;
+        return 0;
+    }
+    else if(n==15)
+    {
+        cout<<"fifteen"<<endl;
+        return 0;
+    }
+    else if(n==16)
+    {
+        cout<<"sixteen"<<endl;
+        return 0;
+    }
+    else if(n==17)
+    {
+        cout<<"seventeen"<<endl;
+        return 0;
+    }
+    else if(n==18)
+    {
+        cout<<"eighteen"<<endl;
+        return 0;
+    }
+    if(n==19)
+    {
+        cout<<"nineteen"<<endl;
+        return 0;
+    }
+    else if(n==10)
+    {
+        cout<<"ten"<<endl;
+        return 0;
+    }
+    if(n==20)
+    {
+        cout<<"twenty"<<endl;
+        return 0;
+    }
+    else if(n==30)
+    {
+        cout<<"thirty"<<endl;
+        return 0;
+    }
+    else if(n==40)
+    {
+        cout<<"forty"<<endl;
+        return 0;
+    }
+    else if(n==50)
+    {
+        cout<<"fifty"<<endl;
+        return 0;
+    }
+    else if(n==60)
+    {
+        cout<<"sixty"<<endl;
+        return 0;
+    }
+    else if(n==70)
+    {
+        cout<<"seventy"<<endl;
+        return 0;
+    }
+    else if(n==80)
+    {
+        cout<<"eighty"<<endl;
+        return 0;
+    }
+    else if(n==90)
+    {
+        cout<<"ninety"<<endl;
+        return 0;
+    }
+    else if(n==12)
+    {
+        cout<<"twelve"<<endl;
+        return 0;
+    }
+    else if(n==11)
+    {
+        cout<<"eleven"<<endl;
+        return 0;
+    }
+    else if(n==2)
+    {
+        cout<<"two"<<endl;
+        return 0;
+    }
+    else if(n==3)
+    {
+        cout<<"three"<<endl;
+        return 0;
+    }
+    else if(n==4)
+    {
+        cout<<"four"<<endl;
+        return 0;
+    }
+    else if(n==5)
+    {
+        cout<<"five"<<endl;
+        return 0;
+    }
+    else if(n==6)
+    {
+        cout<<"six"<<endl;
+        return 0;
+    }
+    else if(n==7)
+    {
+        cout<<"seven"<<endl;
+        return 0;
+    }
+    else if(n==9)
+    {
+        cout<<"nine"<<endl;
+        return 0;
+    }
+    else if(n==8)
+    {
+        cout<<"eight"<<endl;
+        return 0;
+    }
+    else if(n==90)
+    {
+        cout<<"four"<<endl;
+        return 0;
+    }
+    else if(n==0)
+    {
+        cout<<"zero"<<endl;
+    }
+    else if(n==1)
+    {
+        cout<<"one"<<endl;
+    }
+    else if(n>20&&n<100)
+    {
+        if(n/10==2)
+        {
+            cout<<"twenty";
+        }
+        else if(n/10==3)
+        {
+            cout<<"thirty";
+        }
+        else if(n/10==4)
+        {
+            cout<<"forty";
+        }
+        else if(n/10==5)
+        {
+            cout<<"fifty";
+        }
+        else if (n/10==6)
+        {
+            cout<<"sixty";
+        }
+        else if(n/10==7)
+        {
+            cout<<"seventy";
+        }
+        else if(n/10==8)
+        {
+            cout<<"eighty";
+        }
+        else if(n/10==9)
+        {
+            cout<<"ninety";
+        }
+        cout<<"-";
+        if(n%10==1)
+        {
+            cout<<"one"<<endl;
+        }
+        else if(n%10==2)
+        {
+            cout<<"two"<<endl;
+        }
+        else if(n%10==3)
+        {
+            cout<<"three"<<endl;
+        }
+        else if(n%10==4)
+        {
+            cout<<"four"<<endl;
+        }
+        else if(n%10==5)
+        {
+            cout<<"five"<<endl;
+        }
+        else if(n%10==6)
+        {
+            cout<<"six"<<endl;
+        }
+        else if(n%10==7)
+        {
+            cout<<"seven"<<endl;
+        }
+        else if(n%10==8)
+        {
+            cout<<"eight"<<endl;
+        }
+        else if(n%10==9)
+        {
+            cout<<"nine"<<endl;
+        }
+    }
+ 
+    return 0;
+}
