161b6daea32a6b0fd122c6858749f54269089b45
iletavcioski <ilijatavchioski@gmail.com>
2021-08-26 20:00:55 +0200

    shitty day 3

---
+++ CodeForces/CF#739/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+    int n;
+    cin >> n;
+    cout << n << endl;
+    return 0;
+}
